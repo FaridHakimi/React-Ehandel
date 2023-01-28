45adb55d4aa507501f71ffdaf0c77e559e700c28
Farid <F.hakimii09@gmail.com>
2023-01-28 18:54:17 +0100

    working netthandelsside uten betalingsmetode

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ProductProvider } from "./context";
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ProductProvider>
+      <Router>
+        <App /> 
+      </Router>
+    </ProductProvider>
+
   </React.StrictMode>
 );
 
